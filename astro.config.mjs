b8329b39d3297bc77dca630854ac712508bc6a80
timokrieg84-alt <timokrieg84@gmail.com>
2026-07-30 23:42:03 +0200

    chore: astro config with site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
+import { defineConfig } from "astro/config";
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: true })],
+  site: "https://timo-krieg-site.vercel.app",
+  integrations: [],
 });
