481ad9a41d74654a155eb1db38d6283456527cce
timokrieg84-alt <timokrieg84@gmail.com>
2026-07-30 21:40:01 +0200

    chore: enable Tailwind integration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-  site: process.env.SITE_URL || undefined,
-  integrations: [
-    tailwind({
-      config: {
-        applyBaseStyles: true
-      }
-    })
-  ]
+  integrations: [tailwind({ applyBaseStyles: true })],
 });
