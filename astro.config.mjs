ce2c6c5131a1e2935445945e2a496fe286432807
timokrieg84-alt <timokrieg84@gmail.com>
2026-08-05 15:28:06 +0200

    chore(astro): add basic Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: true })],
+  server: { host: true },
+  build: { format: 'directory' },
   vite: {
-    resolve: {
-      alias: {
-        '@': '/src'
-      }
-    }
-  }
+    css: { preprocessorOptions: {} },
+  },
 });
