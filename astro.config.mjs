1095f8e6f904fd9b616fdf9906eff0b8f8b6c32e
timokrieg84-alt <timokrieg84@gmail.com>
2026-08-05 23:01:15 +0200

    chore: add astro.config with site and trailingSlash

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind({ configFile: './tailwind.config.mjs' })],
-  vite: { resolve: { alias: { '@': '/src' } } }
+  site: 'https://timo-krieg-site-git-preview-timo24.vercel.app',
+  trailingSlash: 'never'
 });
