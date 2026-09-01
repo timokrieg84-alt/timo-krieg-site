14673d099402ebdc2617aae8bacb4029ace8f594
timokrieg84-alt <timokrieg84@gmail.com>
2026-09-01 13:56:14 +0200

    configure Astro with Tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
-  integrations: [tailwind({ configFile: './tailwind.config.mjs' })],
-  vite: { resolve: { alias: { '@': '/src' } } }
-});
+  integrations: [
+    tailwind({ applyBaseStyles: false })
+  ]
+})
