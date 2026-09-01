24acd9bf8fa9cec4aa0ace732891ba6572fd718f
timokrieg84-alt <timokrieg84@gmail.com>
2026-09-01 14:12:14 +0200

    chore(build): deterministic astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [
-    tailwind({ applyBaseStyles: false })
-  ]
-})
+  integrations: [tailwind({ configFile: './tailwind.config.mjs' })],
+  vite: { resolve: { alias: { '@': '/src' } } }
+});
